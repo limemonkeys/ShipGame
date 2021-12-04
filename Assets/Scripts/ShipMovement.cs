bb92a263d594dc36d013cb3c0907c7dddf0fb31a
NSFrog <95516477+NSFrog@users.noreply.github.com>
2021-12-04 01:16:35 -0400

    Added new Scripts (to the right spot this time
    > Changed ship movement to feel less icy when turning, 
    > initial ship movement is slightly faster
    >Added initial RusherAI
    >Added initial TurretAI
    >Added initial BossAI

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     public Rigidbody Rigidbody;
+    int currSpeed = 0;
 
     // Start is called before the first frame update
     public void Awake()
@@ -16,19 +17,36 @@ public class ShipMovement : MonoBehaviour
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W)) {
+            if (currSpeed <= 0)
+            {
+                Rigidbody.AddForce(transform.forward * 3);
+                currSpeed += 3;
+            }
             Rigidbody.AddForce(transform.forward * 2);
+            currSpeed += 2;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            Rigidbody.AddForce(transform.forward * -1);
+            Rigidbody.AddForce(transform.forward * -currSpeed);
+            //Rigidbody.AddForce(transform.forward * -1);
             Rigidbody.transform.Rotate(0f, -0.5f, 0f, Space.Self);
+            if (currSpeed >= 2) {
+                Rigidbody.AddForce(transform.forward * (currSpeed - 2));
+                currSpeed = currSpeed - 2;
+            }
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            Rigidbody.AddForce(transform.forward * -1);
+            Rigidbody.AddForce(transform.forward * -currSpeed);
+            //Rigidbody.AddForce(transform.forward * -1);
             Rigidbody.transform.Rotate(0f, 0.5f, 0f, Space.Self);
+            if (currSpeed >= 2)
+            {
+                Rigidbody.AddForce(transform.forward * (currSpeed - 2));
+                currSpeed = currSpeed - 2;
+            }
         }
 
     }
