95e50ebb5ab2c03402173a5114400dd5892b649d
Joseph Burton <js562338@dal.ca>
2021-12-03 23:26:06 -0400

    Commiting existing code before pull

--- Assets/Scripts/BowHitbox.cs
+++ Assets/Scripts/BowHitbox.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BowHitbox : MonoBehaviour
 {
+    public TextMeshProUGUI HealthText;
+    public int maxHealth;
+    public int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,11 @@ public class BowHitbox : MonoBehaviour
         if (other.gameObject.tag == "Enemy") 
         {
             if (other.gameObject.name == "Starboard" || other.gameObject.name == "Port") 
+            {
+                Destroy(other.transform.parent.gameObject);
+            }
+
+            if (other.gameObject.name == "Bow" || other.gameObject.name == "Stern")
             {
                 Destroy(other.transform.parent.gameObject);
             }
